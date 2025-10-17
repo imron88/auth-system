a8788cbae8785e0ffd805e0cf40af166f69d18cc
ronak <ronakpraja340@gmail.com>
2025-10-18 00:05:58 +0530

    signup api

---
+++ app/api/users/signup/route.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request:NextRequest) {
+    try {
+        const reqBody = await request.json()
+        const {username,email,password} = reqBody;
+        
+    } catch (error : any) {
+        console.error("Error in POST /api route:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error", details: error.message },
+      { status: 500 }
+    );
+    }
+
+}
