5918e4b5f11c53358e7006446712eb39ff369b87
ronak <ronakpraja340@gmail.com>
2025-10-17 23:43:33 +0530

    user schema

---
+++ prisma/migrations/20251017175334_init/migration.sql
@@ -0,0 +1,27 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "isVerified" BOOLEAN NOT NULL DEFAULT false,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+    "forgotPasswordToken" TEXT NOT NULL,
+    "forgotPasswordTokenExpiry" TIMESTAMP(3) NOT NULL,
+    "verifyToken" TEXT NOT NULL,
+    "verifyTokenExpiry" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
