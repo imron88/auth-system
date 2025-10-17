427edebe3b3bc063d2279d7b9bd1dbc5d32f2159
ronak <ronakpraja340@gmail.com>
2025-10-17 13:59:46 +0530

    login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,44 @@
+"use client"
+import Link from "next/link";
+import { useState } from "react";
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+const Login = () => {
+    const [user,setUser] = useState({
+        email:"",
+        password:""
+    })
+    const handelLogin = async ()=>{
 
-export default page
+    }
+  return <div>
+
+        <h1>Login</h1>
+        <label htmlFor="email">email</label>
+        <input 
+        type="text" 
+        id="email" 
+        value={user.email}
+        onChange={(e)=>{
+            setUser({...user,email:e.target.value})
+        }}
+        placeholder="email"
+        />
+        <label htmlFor="password">password</label>
+        <input 
+        type="text" 
+        id="password" 
+        value={user.password}
+        onChange={(e)=>{
+            setUser({...user,password:e.target.value})
+        }}
+        placeholder="password"
+        />
+        <button
+        className="p-2 "
+        onClick={handelLogin}
+        >Login</button>
+        <Link href={"/signup"}>SignUp Here</Link>
+    </div>;
+};
+
+export default Login;
