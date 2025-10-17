053a50c25a4eac95a7117c1a14c2a3aed00b4323
ronak <ronakpraja340@gmail.com>
2025-10-18 00:06:26 +0530

    signup api

--- app/api/users/signup/route.ts
+++ app/api/users/signup/route.ts
@@ -4,7 +4,9 @@ export async function POST(request:NextRequest) {
     try {
         const reqBody = await request.json()
         const {username,email,password} = reqBody;
-        
+        console.log('====================================');
+        console.log(reqBody);
+        console.log('====================================');
     } catch (error : any) {
         console.error("Error in POST /api route:", error);
     return NextResponse.json(
