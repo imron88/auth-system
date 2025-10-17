a74093861767b2931c6816c284868784d302153e
ronak <ronakpraja340@gmail.com>
2025-10-17 13:55:38 +0530

    signup page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,7 +1,54 @@
-const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
+"use client"
+import Link from "next/link";
+import { useState } from "react";
 
-export default Signup
+const Signup = () => {
+    const [user,setUser] = useState({
+        username:"",
+        email:"",
+        password:""
+    })
+    const handelSignup = async ()=>{
+
+    }
+  return <div>
+
+        <h1>SignUp</h1>
+        <label htmlFor="username">UserName</label>
+        <input 
+        type="text" 
+        id="username" 
+        value={user.username}
+        onChange={(e)=>{
+            setUser({...user,username:e.target.value})
+        }}
+        placeholder="username"
+        />
+        <label htmlFor="email">email</label>
+        <input 
+        type="text" 
+        id="email" 
+        value={user.email}
+        onChange={(e)=>{
+            setUser({...user,email:e.target.value})
+        }}
+        placeholder="email"
+        />
+        <label htmlFor="password">password</label>
+        <input 
+        type="text" 
+        id="password" 
+        value={user.password}
+        onChange={(e)=>{
+            setUser({...user,password:e.target.value})
+        }}
+        placeholder="password"
+        />
+        <button
+        onClick={handelSignup}
+        >SignUp</button>
+        <Link href={"/login"}>Login Here</Link>
+    </div>;
+};
+
+export default Signup;
