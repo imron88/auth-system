ddb15e816a8129b1ca61c10712c4f46542250d99
ronak <ronakpraja340@gmail.com>
2025-10-18 04:52:55 +0530

    logout page done

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,26 +2,26 @@ import axios from "axios";
 import { useRouter } from "next/navigation";
 
 const Profile = () => {
-  const router = useRouter()
-  const logOut = async()=>{
+  const router = useRouter();
+  const logOut = async () => {
     try {
-      const response = await axios.get("/api/user/logout")
-      console.log('====================================');
-      console.log("logout successfully!",response.data);
-      console.log('====================================');
-      router.push("/login")
+      const response = await axios.get("/api/user/logout");
+      console.log("====================================");
+      console.log("logout successfully!", response.data);
+      console.log("====================================");
+      router.push("/login");
     } catch (error) {
-      console.log('====================================');
+      console.log("====================================");
       console.log(error);
-      console.log('====================================');
+      console.log("====================================");
     }
-  }
+  };
   return (
     <div>
       Profile
       <button onClick={logOut}>LogOut</button>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
