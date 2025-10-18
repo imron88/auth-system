b855de611c3e2774de7a010c74fa8fc56788bb66
ronak <ronakpraja340@gmail.com>
2025-10-18 22:09:41 +0530

    token helper fn

---
+++ helpers/getDataFromToken.ts
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken"
+import { NextRequest } from "next/server"
+
+
+export const getDataFromToken = (request : NextRequest) => {
+    try {
+        const token = request.cookies.get("token")?.value || ""
+        const decode = jwt.verify(token,process.env.TOKEN_SECRET || "")
+        if(!decode){
+            throw new Error("invalid token")
+        }
+        return decode
+    } catch (error : any) {
+        throw new Error(error.message)
+    }
+}
