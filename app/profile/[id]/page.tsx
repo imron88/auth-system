21deacceae2042f827a90739336d33541bcdb873
ronak <ronakpraja340@gmail.com>
2025-10-18 23:19:53 +0530

    user data api

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,18 +1,4 @@
-"use client"
-import axios from "axios";
-import { useState } from "react"
-
 const Profile = () => {
-  const [data,setData] = useState("");
-  const getUserData = async()=>{
-    try {
-      const res = await axios.get("/api/user/data");
-      console.log(res.data);
-      setData(res.data)
-    } catch (error) {
-      console.log(error);
-    }
-  }
   return (
     <div>Profile</div>
   )
