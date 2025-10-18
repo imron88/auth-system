eac6ab3b92009937f9203d4ab7053e80c9fe6e4e
ronak <ronakpraja340@gmail.com>
2025-10-18 23:28:10 +0530

    profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -10,7 +10,7 @@ const Profile = () => {
     try {
       const res = await axios.get("/api/user/data");
       console.log(res.data);
-      setData(res.data)
+      setData(res.data.user)
     } catch (error) {
       console.log(error);
     }
@@ -32,7 +32,7 @@ const Profile = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-900 via-gray-800 to-black">
       <div className="bg-gray-900 p-8 rounded-2xl shadow-2xl w-full max-w-md border border-gray-700 text-center">
-        <h1 className="text-3xl font-bold text-white mb-6">{data}</h1>
+        <h1 className="text-3xl font-bold text-white mb-6">{data.username}</h1>
         <p className="text-gray-300 mb-6">Welcome to your dashboard!</p>
 
         <button
