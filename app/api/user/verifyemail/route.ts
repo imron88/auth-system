af3a95f924361e5eda4570dfcbf53acc2f04fbe5
ronak <ronakpraja340@gmail.com>
2025-10-26 23:49:59 +0530

    verify email route catch

--- app/api/user/verifyemail/route.ts
+++ app/api/user/verifyemail/route.ts
@@ -1,5 +1,12 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST( request : NextRequest) {
-    
+export async function POST(request: NextRequest) {
+  try {
+  } catch (error: any) {
+    console.error("Error in POST /api route:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error", details: error.message },
+      { status: 500 }
+    );
+  }
 }
