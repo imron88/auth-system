c9d4904a26f6faa052123abde7a6ed5c059b0b7c
ronak <ronakpraja340@gmail.com>
2025-10-27 01:24:21 +0530

    verify email route done

--- app/api/user/verifyemail/route.ts
+++ app/api/user/verifyemail/route.ts
@@ -3,8 +3,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   try {
-    const reqBody = request.json()
-    const {token} = reqBody;
+    const reqBody = request.json();
+    const { token } = reqBody;
     console.log(token);
     if (!token) {
       return NextResponse.json(
@@ -13,11 +13,11 @@ export async function POST(request: NextRequest) {
       );
     }
     const user = await prisma.user.findFirst({
-        where : {
-            verifyToken :token,
-            verifyTokenExpiry : {gt: new Date()}
-        }
-    })
+      where: {
+        verifyToken: token,
+        verifyTokenExpiry: { gt: new Date() },
+      },
+    });
     if (!user) {
       return NextResponse.json(
         { error: "Invalid or expired token" },
@@ -36,7 +36,6 @@ export async function POST(request: NextRequest) {
       message: "Email verified successfully!",
       success: true,
     });
-
   } catch (error: any) {
     console.error("Error in POST /api route:", error);
     return NextResponse.json(
